504645d9352f1ebff3b616cf0f3f02efe59e9923
paiva <5972140@gmail.com>
2022-05-06 09:03:51 +0300

    refactor: read variables from environment, put some constants into .env file

--- api_server/celery_app.py
+++ api_server/celery_app.py
@@ -1,9 +1,11 @@
+import os
+
 from celery import Celery
 
 app = Celery(
     "retraining",
-    backend="redis://redis/1",
-    broker="redis://redis/0",
+    backend=os.environ.get('RESULT_BACKEND'),
+    broker=os.environ.get('BROKER_URL'),
 )
 
 app.autodiscover_tasks()
