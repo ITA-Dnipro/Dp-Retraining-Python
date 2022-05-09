1bc0eea48977103de1f687a2acbf00586b6b84b7
Bohdan Suslovych <suslovichb@gmail.com>
2022-05-09 16:13:04 +0300

    Add frontend skeleton

---
+++ frontend/src/components/Charities.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Charities = () => {
+    return (
+        <div>
+            Charities content
+        </div>
+    )
+}
+
+export default Charities;
---
+++ frontend/src/components/Posts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Posts = () => {
+    return (
+        <div>
+            Posts content
+        </div>
+    )
+}
+
+export default Posts;
