66248c22540c610a4416ecc71c38e9d4fee6da9c
Bohdan Suslovych <suslovichb@gmail.com>
2022-05-17 08:51:02 +0300

    Fix main application header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
-import Header from './components/Navbar';
+import Navbar from './components/Navbar';
+import Auth from './components/Auth'
+import Posts from './components/Posts';
+import Charities from './components/Charities';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <div className="App">
         <BrowserRouter>
-            <Header />
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/posts" element={<Posts />} />
+                <Route path="/charities" element={<Charities />} />
+                <Route path="/auth" element={<Auth />} />
+            </Routes>
         </BrowserRouter>
     </div>
   );
--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import {Link, NavLink} from 'react-router-dom';
 
 const Header = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
         <Container fluid>
-          <Navbar.Brand href="/">DONATello</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">DONATello</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="m-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/charities">Charities</Nav.Link>
-              <Nav.Link href="/posts">Posts</Nav.Link>
+              <Nav.Link as={NavLink} to="/" exact>Home</Nav.Link>
+              <Nav.Link as={NavLink} to="/charities">Charities</Nav.Link>
+              <Nav.Link as={NavLink} to="/posts">Posts</Nav.Link>
             </Nav>
-            <Button href="/auth/login" variant="outline-light" >Sing In</Button>
+            <Button as={Link} to="/auth" variant="outline-light" >Sing In</Button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
@@ -22,4 +23,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
