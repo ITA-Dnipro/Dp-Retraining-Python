88da4a6d0a7f17257e67038069d34e0f2a572b34
Maksym Shvets <74673988+MaksymShvets@users.noreply.github.com>
2022-05-17 11:15:36 +0300

    Create dummy User Profile (#30)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import './App.css';
 import Header from './components/Navbar';
+import Profile from './components/Profile/Profile';
 
 const App = () => {
   return (
     <div className="App">
         <BrowserRouter>
             <Header />
+            <Profile />
         </BrowserRouter>
     </div>
   );
