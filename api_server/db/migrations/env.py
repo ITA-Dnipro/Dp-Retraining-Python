4721dd76a1a1cdb7d0557c925209d709100bcd27
maklabas <5972140@gmail.com>
2022-05-19 11:02:11 +0300

    add migrations

--- api_server/db/migrations/env.py
+++ api_server/db/migrations/env.py
@@ -3,12 +3,12 @@ from logging.config import fileConfig
 
 from alembic import context
 from app.config import get_app_config
-from charity.models import CharityOrganisation, CharityUserAssociation
 from common.constants.api import ApiConstants
 from db import Base
 from sqlalchemy import engine_from_config, pool
 from sqlalchemy.ext.asyncio import AsyncEngine
 from users.models import User
+from charity.models import CharityOrganisation, CharityUserAssociation
 
 Config = get_app_config(ApiConstants.DEVELOPMENT_CONFIG.value)
 
