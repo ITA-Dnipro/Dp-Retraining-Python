4b7eba4c9acae531187d50a88c38714ccc133c47
Maksym Shvets <maxshvets1901@gmail.com>
2022-05-20 14:45:55 +0300

    Minor style changes

--- frontend/src/components/Charity/Charity.jsx
+++ frontend/src/components/Charity/Charity.jsx
@@ -10,7 +10,7 @@ const Charity = () => {
                     <div className="charityInfo">
                         <span className="charityTitle">Charity Title</span>
                         <span className="charityDescriptionKey">Description:</span>
-                        <span className="charityDescriptionValue">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dolor inventore dolores libero, minima sunt labore at aut ipsum accusantium quas distinctio, veniam molestiae quasi! Repudiandae quas ab eius animi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium at expedita alias accusamus illum ut! Non nisi odio sequi cumque aperiam tempora, suscipit porro voluptas repellat debitis distinctio, quia quidem.</span>
+                        <span className="charityDescriptionValue">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dolor inventore dolores libero, minima sunt labore at aut ipsum accusantium quas distinctio, veniam molestiae quasi! Repudiandae quas ab eius animi! Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
                         <span className="email"><b>Email:</b> testcharity@gmail.com</span>
                         <span className="phone"><b>Phone number:</b> +380987654321</span>
                     </div>
