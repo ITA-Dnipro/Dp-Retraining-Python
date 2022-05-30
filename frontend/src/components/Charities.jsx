ab04796c07960718c61355b5f83c40c388525415
Bohdan Suslovych <suslovichb@gmail.com>
2022-05-30 09:45:51 +0300

    Add charities list loading

--- frontend/src/components/Charities.jsx
+++ frontend/src/components/Charities.jsx
@@ -1,52 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
+import axiosInstance from "../axiosApi";
 
 const Charities = () => {
-    const [charitiesState, setCharitiesState] = useState([
-        {
-            "id": "c12",
-            "title": "Charity 1",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "phone_number": "+380394675559",
-            "organisation_email": "c1@mail.com"
-        },
-        {
-            "id": "c2",
-            "title": "Charity 2",
-            "description": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "phone_number": "+380394679999",
-            "organisation_email": "c2@mail.com"
-        },
-        {
-            "id": "c3",
-            "title": "Charity 3",
-            "description": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "phone_number": "+380394678888",
-            "organisation_email": "c3@mail.com"
-        },
-        {
-            "id": "c4",
-            "title": "Charity 4",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "phone_number": "+380394678888",
-            "organisation_email": "c4@mail.com"
-        }
-    ]);
+    const [charitiesState, setCharitiesState] = useState([]);
 
     useEffect(() => {
         loadCharities();
     }, []);
 
     const loadCharities = () => {
+        axiosInstance.get(
+            `charities/`
+            )
+            .then(response => {
+                setCharitiesState(response.data.data);
+                console.log(response.data.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     const charityCard = (id, title, description, phone_number, email) => {
         return (
-            <div key={id} className="charity-card mx-auto my-4 border border-primary rounded">
-                <div>{id}</div>
-                <div>{title}</div>
-                <div>{description}</div>
-                <div>{phone_number}</div>
-                <div>{email}</div>
+            <div key={id} className="charity-card d-flex flex-row mx-auto my-4 border border-primary rounded">
+                <div className="col-3 mx-auto">
+                </div>
+                <div className="col-8 mx-auto">
+                    <div className="text-left"><h3>{title}</h3></div>
+                    <Link to={"/charities/"+id}>Details</Link>
+                </div>
             </div>
         );
     }
