78500b6b7aad8d7e1561105d6d073780342fa5a3
Andrii Kovalov <jfkwww1@gmail.com>
2022-07-04 13:23:22 +0300

    Basic smoke tests for POST '/fundraisers/{id}/statuses' endpoint added.

---
+++ api_server/common/tests/test_data/fundraisers/request_test_fundraise_status_data.py
@@ -0,0 +1,15 @@
+ADD_FUNDRAISE_STATUS_IN_PROGRESS_TEST_DATA = {
+    'name': 'In progress',
+}
+ADD_FUNDRAISE_STATUS_ON_HOLD_TEST_DATA = {
+    'name': 'On hold',
+}
+ADD_FUNDRAISE_STATUS_COMPLETED_TEST_DATA = {
+    'name': 'Completed',
+}
+ADD_FUNDRAISE_STATUS_NEW_TEST_DATA = {
+    'name': 'New',
+}
+ADD_FUNDRAISE_STATUS_NOT_SUPPORTED_TEST_DATA = {
+    'name': 'Not supported',
+}
